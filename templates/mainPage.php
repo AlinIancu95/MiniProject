5b824341503fa42c7a6322aab0bc966b223968a5
Alin <alin.iancu95@gmail.com>
2022-03-08 11:21:54 +0200

    mainpage created and db connection

---
+++ templates/mainPage.php
@@ -0,0 +1,122 @@
+<?php include "header.php" ?>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 headMainPage">
+                <div class="row">
+                    <div class="col-9">
+                        <h3>Product List</h3>
+                    </div>
+                    <div class="col-1">
+                        <a href="#" class="btn btn-primary">
+                            ADD
+                        </a>
+                    </div>
+                    <div class="col-2">
+                        <button class="btn btn-primary" id="delete-product-btn">
+                            MASS DELETE
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col"><hr></div>
+        </div>
+        <div class="row">
+            <div class="col bodyMainPage">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="card cardMain">
+                            <div>
+                                <label></label>
+                                <input type="checkbox" class="delete-checkbox">
+                            </div>
+                            <div class="card-body cardBody">
+                                <span>SKU</span>
+                                <br>
+                                <span>Name</span>
+                                <br>
+                                <span>Price</span>
+                                <br>
+                                <span>Type</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card cardMain">
+                            <div>
+                                <label></label>
+                                <input type="checkbox" class="delete-checkbox">
+                            </div>
+                            <div class="card-body cardBody">
+                                <span>SKU</span>
+                                <br>
+                                <span>Name</span>
+                                <br>
+                                <span>Price</span>
+                                <br>
+                                <span>Type</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card cardMain">
+                            <div>
+                                <label></label>
+                                <input type="checkbox" class="delete-checkbox">
+                            </div>
+                            <div class="card-body cardBody">
+                                <span>SKU</span>
+                                <br>
+                                <span>Name</span>
+                                <br>
+                                <span>Price</span>
+                                <br>
+                                <span>Type</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card cardMain">
+                            <div>
+                                <label></label>
+                                <input type="checkbox" class="delete-checkbox">
+                            </div>
+                            <div class="card-body cardBody">
+                                <span>SKU</span>
+                                <br>
+                                <span>Name</span>
+                                <br>
+                                <span>Price</span>
+                                <br>
+                                <span>Type</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card cardMain">
+                            <div>
+                                <label></label>
+                                <input type="checkbox" class="delete-checkbox">
+                            </div>
+                            <div class="card-body cardBody">
+                                <span>SKU</span>
+                                <br>
+                                <span>Name</span>
+                                <br>
+                                <span>Price</span>
+                                <br>
+                                <span>Type</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col"><hr></div>
+        </div>
+    </div>
+</body>
+</html>
